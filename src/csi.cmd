e3d38bbdb411720231ad93e79b56a09621c3dadc
Atif Aziz <code@raboof.com>
2016-06-29 10:16:11 +0200

    Short word on getting NuGet (when missing) in genned scripts

--- src/csi.cmd
+++ src/csi.cmd
@@ -14,13 +14,25 @@ popd
 if %errorlevel%==0 "%CSIPATH%" "%~dpn0.csx" -- %*
 goto :EOF
 
-:pkgerr
-popd
-exit /b %errorlevel%
-
 :nocsi
 >&2 echo Microsoft (R) Visual C# Interactive Compiler does not appear to be
 >&2 echo installed. You can download it as part of Microsoft Build Tools 2015
 >&2 echo using the URL below, install and try again:
 >&2 echo https://www.microsoft.com/en-us/download/details.aspx?id=48159
+exit /b 1
+
+:pkgerr
+set err=%errorlevel%
+set NUGETPATH=
+for %%i in (nuget.exe) do set NUGETPATH=%%~$PATH:i
+if not defined NUGETPATH call :nonuget
+popd
+exit /b %err%
+
+:nonuget
+>&2 echo NuGet does not appear to be installed, which is needed to restore
+>&2 echo missing dependencies. Visit the following URLs for instructions on how
+>&2 echo to obtain the command-line version of NuGet:
+>&2 echo - https://docs.nuget.org/consume/installing-nuget#command-line-utility
+>&2 echo - https://dist.nuget.org/index.html
 exit /b 1
--- src/exe.cmd
+++ src/exe.cmd
@@ -8,5 +8,17 @@ if %errorlevel%==0 "%~dpn0.exe" %*
 goto :EOF
 
 :pkgerr
+set err=%errorlevel%
+set NUGETPATH=
+for %%i in (nuget.exe) do set NUGETPATH=%%~$PATH:i
+if not defined NUGETPATH call :nonuget
 popd
-exit /b %errorlevel%
+exit /b %err%
+
+:nonuget
+>&2 echo NuGet does not appear to be installed, which is needed to restore
+>&2 echo missing dependencies. Visit the following URLs for instructions on how
+>&2 echo to obtain the command-line version of NuGet:
+>&2 echo - https://docs.nuget.org/consume/installing-nuget#command-line-utility
+>&2 echo - https://dist.nuget.org/index.html
+exit /b 1
