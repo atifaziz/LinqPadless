4b8488103bde391b4c3d61cbac7a0952e88fe87d
Atif Aziz <code@raboof.com>
2017-03-28 18:39:37 +0200

    Don't upgrade indirect dependencies to latest version; closes #21
    This uses the same policy of lowest version as NuGet 3.x.

--- src/NuGet.cs
+++ src/NuGet.cs
@@ -22,10 +22,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -33,7 +33,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-//    
+//
 #endregion
 
 namespace LinqPadless.NuGet
@@ -161,7 +161,7 @@ namespace LinqPadless.NuGet
                                 select dir.ResolvePackages(d.Id, targetFramework, this, CancellationToken.None).Result
                                           .Where(spdi => spdi.HasVersion && d.VersionRange.Satisfies(spdi.Version))
                                           .DefaultIfEmpty()
-                                          .MaxBy(sdpi => sdpi?.Version)
+                                          .MinBy(sdpi => sdpi?.Version)
                                 into dp
                                 where dp != null
                                 from r in GetReferencesTree(FindInstalledPackage(dp.Id, dp.Version, dp.Version.IsPrerelease), targetFramework, writer?.Indent())
