d0205ac7b58f0619bef63b2859ab7a87dd1f8173
Atif Aziz <code@raboof.com>
2018-10-05 18:31:43 +0200

    Fix dotnet-script name in error message

--- src/dotnet-script.cmd
+++ src/dotnet-script.cmd
@@ -9,7 +9,7 @@ call "%DOTNET_SCRIPT_PATH%" "%~dpn0.csx" -- %*
 goto :EOF
 
 :no-dotnet-script
->&2 echo dotnet does not appear to be installed, which is needed to run this
->&2 echo script. For instructions on how to download and install, visit:
->&2 echo https://github.com/filipw/dotnet-script
+>&2 echo dotnet-script does not appear to be installed, which is needed to
+>&2 echo run this script. For instructions on how to download and install,
+>&2 echo visit: https://github.com/filipw/dotnet-script
 exit /b 1
