389a4ba0113c656d6875c032cd72a56c7b3a1dae
Atif Aziz <code@raboof.com>
2018-10-17 11:43:15 +0200

    Fix Bash scripts to CD into script dir

--- pack.sh
+++ pack.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-[[ -e pack.sh ]] || { echo >&2 "Please cd into the script location before running it."; exit 1; }
 set -e
+cd "$(dirname "$0")"
 if [ -n "$1" ]; then
     VERSION_SUFFIX="--version-suffix $1"
 else
