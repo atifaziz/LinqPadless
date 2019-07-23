4687bbf806a2e6f774f22efbc3ed8801841b631e
Atif Aziz <code@raboof.com>
2019-07-23 15:54:23 +0200

    Make build scripts quieter (no `dotnet --info`)

--- build.cmd
+++ build.cmd
@@ -6,7 +6,6 @@ goto :EOF
 
 :main
     dotnet restore ^
- && dotnet --info ^
  && dotnet build --no-restore -c Debug ^
  && dotnet build --no-restore -c Release
 goto :EOF
--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname "$0")"
-dotnet --info
 dotnet restore
 for c in Debug Release; do
     dotnet build --no-restore -c $c
