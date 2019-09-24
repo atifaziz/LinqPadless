40088e38ba0ee7e24ddc353a4c4268fc1c80ccf8
Atif Aziz <code@raboof.com>
2019-09-24 17:56:48 +0200

    Revert new targets (2.2 & 3.0)
    These will be brought back in a separate commit.

--- publish.sh
+++ publish.sh
@@ -2,8 +2,4 @@
 set -e
 cd "$(dirname "$0")"
 ./build.sh
-for f in netcoreapp2.1 \
-         netcoreapp2.2 \
-         netcoreapp3.0; do
-    dotnet publish --no-restore --no-build -c Release -f $f -o dist/bin/$f src
-done
+dotnet publish --no-restore --no-build -c Release -o ../dist/bin src
