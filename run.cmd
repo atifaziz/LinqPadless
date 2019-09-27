a8d2f7e5dcbdcf72c61d66f5309ba46f82187c1b
Atif Aziz <code@raboof.com>
2019-09-27 15:14:31 +0200

    Add .NET Core App 2.2 & 3 as targets (#33)

--- run.cmd
+++ run.cmd
@@ -1,5 +1,5 @@
 @echo off
 pushd "%~dp0"
 echo>&2 WARNING! Working directory is: %~dp0
-dotnet run --no-launch-profile -p src -- %*
+dotnet run --no-launch-profile -f netcoreapp3.0 -p src -- %*
 popd
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -e
 cd "$(dirname "$0")"
 echo>&2 "WARNING! Working directory is: $(pwd)"
-dotnet run --no-launch-profile -p src -- "$@"
+dotnet run --no-launch-profile -f netcoreapp3.0 -p src -- "$@"
