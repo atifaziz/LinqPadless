b680b0368e81fcb9961fd3eed01d2c664300b1ac
Atif Aziz <code@raboof.com>
2020-01-30 18:56:40 +0100

    Fix target framework in publish scripts [ci skip]

--- publish.cmd
+++ publish.cmd
@@ -4,7 +4,7 @@ pushd "%~dp0"
 call build
 set EXIT_CODE=%ERRORLEVEL%
 if not %EXIT_CODE%==0 goto :end
-for %%f in (3.0 2.2 2.1) do (
+for %%f in (3.1 2.1) do (
     dotnet publish --no-restore --no-build -c Release -f netcoreapp%%f -o dist\bin\%%f src || goto :break
 )
 :break
--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@
 set -e
 cd "$(dirname "$0")"
 ./build.sh
-for f in 3.0 2.2 2.1; do
+for f in 3.1 2.1; do
     dotnet publish --no-restore --no-build -c Release -f netcoreapp$f -o dist/bin/$f src
 done
